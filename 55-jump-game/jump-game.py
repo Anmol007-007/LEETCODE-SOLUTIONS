044519857b97961cf1780c7ada76fe65a663be23
Anmol007-007 <anmolsoniaids09@gmail.com>
2026-07-16 13:01:39 +0530

    Time: 14 ms (81.95%) | Memory: 20.2 MB (85.32%) - LeetSync

---
+++ 55-jump-game/jump-game.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        curr= 0
+        for i in range(len(nums)):
+            if i>curr:
+                return False
+            curr =max( curr , (i+nums[i]))
+            if curr>=len(nums)-1:
+                return True
+        return True
