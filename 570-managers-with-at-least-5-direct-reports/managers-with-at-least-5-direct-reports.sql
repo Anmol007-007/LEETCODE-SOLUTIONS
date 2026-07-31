1f333d180a60aa411ffb6ad84901edca48dde773
Anmol007-007 <anmolsoniaids09@gmail.com>
2026-07-31 11:53:38 +0530

    Time: 378 ms (38.83%) | Memory: 0B (100.00%) - LeetSync

---
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,15 @@
+select 
+    name 
+from 
+    employee 
+where 
+    id 
+in 
+    (select 
+        managerid 
+    from 
+        employee 
+    group by 
+        managerid 
+    having 
+        count(*)>=5)
