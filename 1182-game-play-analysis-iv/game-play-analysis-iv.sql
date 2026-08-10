8a78b091ee8262010a2beb0db17b7d1d341dc891
Anmol007-007 <anmolsoniaids09@gmail.com>
2026-08-10 09:11:00 +0530

    Time: 614 ms (45.60%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,8 @@
+select 
+    round(count(distinct a1.player_id)/(select count(distinct player_id) from activity),2) as fraction
+from 
+    activity a1 join activity a2 
+    on a1.player_id=a2.player_id
+    and datediff(a2.event_date,a1.event_date)=1
+where 
+    (a1.player_id,a1.event_date) in (select player_id,min(event_date) from activity group by player_id)
