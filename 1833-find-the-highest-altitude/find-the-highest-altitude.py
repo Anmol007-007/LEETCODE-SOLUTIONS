741efdf2721d3ba30a000301026fd10b7494740e
Anmol007-007 <anmolsoniaids09@gmail.com>
2026-09-17 12:06:05 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (52.81%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.py
@@ -0,0 +1,8 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        ans=[0]
+        s=0
+        for i in range(len(gain)):
+            s+=gain[i]
+            ans.append(s)
+        return max(ans)
